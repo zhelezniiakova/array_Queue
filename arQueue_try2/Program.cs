feb54c3c7d6fc63e548e83348c2d190ff213dbe8
zhelezniiakova <67381768+zhelezniiakova@users.noreply.github.com>
2020-06-24 20:42:15 +0300

    Add files via upload

---
+++ arQueue_try2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using arLib2;
+
+namespace arQueue_try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            ArrayQueue<string> arr = new ArrayQueue<string>(count);
+            string s = "cfvfd";
+            for (int i = 0; i < count; i++)
+            {
+                arr.Enqueue("элемент" + i);
+            }
+            IEnumerator<string> el1 = arr.GetEnumerator();
+            arr.Enqueue(s);
+            arr.Enqueue(s);
+            arr.Enqueue(s);
+            arr.Enqueue(s);
+            foreach (string el in arr)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine("//--//--//");
+            Console.WriteLine();
+       
+            Console.WriteLine(arr.Dequeue());
+           // Console.WriteLine(arr.Peek());
+            // arr.Clear();
+            Console.ReadLine();
+        }
+    }
+}
